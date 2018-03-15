f5476138928062322ecbeb9a099d76fa2369a938
Ashvini <ashvinikumar45@gmail.com>
2018-03-15 13:16:46 +0530

    alignment set according to design

--- src/component/question/index.js
+++ src/component/question/index.js
@@ -3,17 +3,18 @@ import OptionList from '../optionList';
 
 function Question({ question, onSkip }){
     console.log(question);
-    return (<div>
-            <div>
-                {question.title}
-                {question.text}
-            </div>
+    return (<div style={{width: "580px", "marginLeft": "50px" }}>
+                <div>
+                    <h2>{question.title}:</h2>
+                    {question.text}
+                </div>
+                <br />
 
-            <div>
-                <OptionList options={question.options} />
-                <button className="button" onClick={() => onSkip(question.id)}>Skip</button>
+                <div>
+                    <OptionList options={question.options} style={{float: "right"}} /><br />
+                    <button className="button" onClick={() => onSkip(question.id)} style={{float: "right"}}>Skip</button>
+                </div>
             </div>
-        </div>
     );
 }
 
